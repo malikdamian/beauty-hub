3e17feeff869af168cee942530b2a3b0528a0416
Damian Malik <malikdamiann@gmail.com>
2025-01-10 22:39:43 +0100

    add drf-spectacular for API schema generation and documentation

--- beauty_hub/settings.py
+++ beauty_hub/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework'
+    'rest_framework',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Beauty Hub',
+    'DESCRIPTION': 'Example description',
+    'VERSION': '0.0.1',
+}
