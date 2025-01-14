e649cf650376f807e561c23acf60ab98bf7ecb51
Damian Malik <malikdamiann@gmail.com>
2025-01-14 22:29:23 +0100

    add Service and Category models

--- service/models.py
+++ service/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self) -> str:
+        return f"Category: {self.name}"
+
+
+class Service(models.Model):
+    category = models.OneToOneField(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=1024)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    time = models.FloatField()
+    seat = models.CharField(max_length=128, null=True)
+    note = models.CharField(max_length=2048, null=True)
+
+    def __str__(self) -> str:
+        return f"Service: {self.name}"
