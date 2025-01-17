aeb5309d6d53fbfbd93a38d1b3d43dec07c4307a
Damian Malik <malikdamiann@gmail.com>
2025-01-17 22:34:27 +0100

    update Swagger path

--- beauty_hub/urls.py
+++ beauty_hub/urls.py
@@ -22,9 +22,11 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
     SpectacularRedocView,
 )
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
