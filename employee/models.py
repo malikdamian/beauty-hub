a70ce421452fba6b70c357043c7c59d5c5febbf4
Damian Malik <malikdamiann@gmail.com>
2025-01-21 23:11:18 +0100

    add Employee model and inital migration

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Employee(AbstractUser):
+    phone = models.CharField(max_length=15, null=True)
+    specialization = models.CharField(max_length=128, null=True)
+    photo = models.BinaryField(null=True)
+    modified_at = models.DateTimeField(auto_now=True)
