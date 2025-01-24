1c83ee98233c916169b5a33c92ef2a985bd1dfe2
Damian Malik <malikdamiann@gmail.com>
2025-01-24 23:16:32 +0100

    add URL routing for Employee app

--- beauty_hub/urls.py
+++ beauty_hub/urls.py
@@ -16,17 +16,17 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from drf_spectacular.views import (
     SpectacularAPIView,
-    SpectacularSwaggerView,
     SpectacularRedocView,
+    SpectacularSwaggerView,
 )
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('service.urls')),
+    path('', include('employee.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
