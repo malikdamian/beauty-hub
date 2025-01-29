bae0d74614f6ef0987f814e7e7db85dc574bc53b
Damian Malik <malikdamiann@gmail.com>
2025-01-29 22:38:35 +0100

    add Customer model

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from service.models import TimeStampMixin
+
+
+class Customer(TimeStampMixin):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    email = models.EmailField(null=True)
+    note = models.CharField(max_length=512, null=True)
